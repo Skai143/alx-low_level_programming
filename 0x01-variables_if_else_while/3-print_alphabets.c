f062285833f6eb081fbc194a73a4f445bc4ada62
Njemanze Harvey <njemanzeharvey@gmail.com>
2023-03-20 16:31:02 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char y;
+
+	for (y = 'a' ; y <= 'z' ; y++)
+		putchar(i);
+	for (y = 'A' ; y <= 'Z' ; y++);
+		putchar(i);
+	putchar('\n');
+	return (0);	
+}

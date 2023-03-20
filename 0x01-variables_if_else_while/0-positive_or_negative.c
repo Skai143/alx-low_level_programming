e4cfbf1f13bfc624d0562f4dda056cdb24ca273b
Njemanze Harvey <njemanzeharvey@gmail.com>
2023-03-20 14:18:02 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,7 +6,7 @@
 /**
  * main - main function
  *
- * Return: 0
+ * Return: always 0
  */
 
 int main(void)
@@ -20,7 +20,7 @@ int main(void)
 		printf("%d is positive", n);
 	else if (n == 0)
 		printf("%d is zero", n);
-	else if (n < 0)
+	else
 		printf("%d is negative", n);
 	return (0);
 }

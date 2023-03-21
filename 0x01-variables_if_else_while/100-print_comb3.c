601466909437f8c4c9478c9036b7eb3cf9f37403
Njemanze Harvey <njemanzeharvey@gmail.com>
2023-03-20 19:12:33 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,10 +9,23 @@
 int main(void)
 {
 	int y;
+	int z;
 
 	for (y = 0 ; y < 10 ; y++)
 	{
-		putchar(y + '0');
+		for (z = 1 ; z < 10 ; z++)
+		{
+			if (y < z && y != z)
+			{
+				putchar(y + '0');
+				putchar(z + '0');
+				if (y + z != 17)
+				{
+					putchar(",");
+					putchar(' ');
+				}
+			}
+		}
 	}
 	putchar('\n');
 	return (0);

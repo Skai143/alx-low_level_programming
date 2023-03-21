583f37c3f4922bae83d490020f463b23b174f773
Njemanze Harvey <njemanzeharvey@gmail.com>
2023-03-20 17:13:38 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int y;
+
+	for (y = 0 ; y < 10 ; y++)
+		putchar(y);
+	putchar("\n");
+	return (0)
+}

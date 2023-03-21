b7b1e10f9fab55c1c94d1ce9ad570396a6e40135
Njemanze Harvey <njemanzeharvey@gmail.com>
2023-03-20 17:01:59 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *main - main function
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int y;
+	for (y = 0 ; y < 10 ; y++)
+		printf("%d", y);
+	printf("\n");
+	return (0);
+}

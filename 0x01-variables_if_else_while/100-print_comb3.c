1191c5875dc58cb1bc26c7ae0597d2561c58c0a8
Njemanze Harvey <njemanzeharvey@gmail.com>
2023-03-20 18:53:43 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - main function
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int y;
+
+	for (y = 0 ; y < 10 ; y++)
+	{
+		putchar(y + '0');
+	}
+	putchar('\n');
+	return (0);
+}

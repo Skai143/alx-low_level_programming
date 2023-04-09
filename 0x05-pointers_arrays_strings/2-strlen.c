19bfd12f9a6317b99cd29fb1c0479696873d53fc
Njemanze Harvey <njemanzeharvey@gmail.com>
2023-04-09 16:13:27 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,17 +3,14 @@
  *int _strlen: returns the length of a string
  *@s: this is the character name
  *
- *Return: Always 0.
+ *Return: Always 0 as successful.
  */
 int _strlen(char *s);
 
 {
 	int lt = 0;
 
-	while (*s != '\0')
-	{
-		lt++;
-		*s++;
-	}
+	while (*s++)
+	lt++;
 	return lt;
 }
